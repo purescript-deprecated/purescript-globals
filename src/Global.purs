f92e074c4fbac1a63dc116b208e51656442556a9
Gary Burgess <gary.burgess@gmail.com>
2014-03-19 20:50:14 +0000

    Initial commit

---
+++ src/Global.purs
@@ -0,0 +1,45 @@
+module Global where
+
+foreign import nan "var nan = NaN;" :: Number
+
+foreign import infinity "var infinity = Infinity;" :: Number
+
+foreign import toExponential 
+  "function toExponential(n) {\
+  \  return n.toExponential();\
+  \}" :: Number -> String
+
+foreign import toFixed 
+  "function toFixed(d) {\
+  \  return function(n) {\
+  \    return n.toFixed(d);\
+  \  };\
+  \}" :: Number -> Number -> String
+
+foreign import toPrecision 
+  "function toPrecision(d) {\
+  \  return function(n) {\
+  \    return n.toPrecision(d);\
+  \  };\
+  \}" :: Number -> Number -> String
+
+foreign import isFinite :: Number -> Boolean
+
+foreign import parseFloat :: String -> Number
+
+foreign import parseInt 
+  "function parseInt(n) {\
+  \  return function(radix) {\
+  \    return parseInt(n, radix);\
+  \  };\
+  \}" :: String -> Number -> Number
+
+foreign import encodeURIComponent :: String -> String
+
+foreign import decodeURIComponent :: String -> String
+
+foreign import encodeURI :: String -> String
+
+foreign import decodeURI :: String -> String
+
+foreign import isNaN :: Number -> Boolean
