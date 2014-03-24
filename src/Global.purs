680106ae9e429a8af7b11ff03b2cf9670c825a97
joneshf <jones3.hardy@gmail.com>
2014-03-24 09:14:27 -0700

    Add better typeof.

--- src/Global.purs
+++ src/Global.purs
@@ -4,19 +4,19 @@ foreign import nan "var nan = NaN;" :: Number
 
 foreign import infinity "var infinity = Infinity;" :: Number
 
-foreign import toExponential 
+foreign import toExponential
   "function toExponential(n) {\
   \  return n.toExponential();\
   \}" :: Number -> String
 
-foreign import toFixed 
+foreign import toFixed
   "function toFixed(d) {\
   \  return function(n) {\
   \    return n.toFixed(d);\
   \  };\
   \}" :: Number -> Number -> String
 
-foreign import toPrecision 
+foreign import toPrecision
   "function toPrecision(d) {\
   \  return function(n) {\
   \    return n.toPrecision(d);\
@@ -27,7 +27,7 @@ foreign import isFinite :: Number -> Boolean
 
 foreign import parseFloat :: String -> Number
 
-foreign import parseInt 
+foreign import parseInt
   "function parseInt(n) {\
   \  return function(radix) {\
   \    return parseInt(n, radix);\
@@ -43,3 +43,8 @@ foreign import encodeURI :: String -> String
 foreign import decodeURI :: String -> String
 
 foreign import isNaN :: Number -> Boolean
+
+foreign import typeof'
+  "function typeof$prime(x) {\
+  \  return {}.toString.call(x).slice(8, -1);\
+  \}" :: forall a. a -> String
